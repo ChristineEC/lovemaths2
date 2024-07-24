f4ebe052180eae0ecc83be21c4585639cf9a1467
Christine Carter <pxe50zgd@students.codeinstitute.net>
2024-07-24 19:26:05 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
+function runGame() {
 
+}
+
+function checkAnswer() {
+
+}
+
+function calculateAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
